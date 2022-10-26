b5ed8f33ec00aa39d6f0a156f8594a3ea1387cd1
Sakshi Bisen <sakshi.bisen45@gmail.com>
2022-10-27 00:56:49 +0530

    create pages and routes

--- rentals/src/App.js
+++ rentals/src/App.js
@@ -1,10 +1,24 @@
-
-
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
 function App() {
   return (
-    <div >
-      <h1>hemllo</h1>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<Signin/>}/>
+        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
