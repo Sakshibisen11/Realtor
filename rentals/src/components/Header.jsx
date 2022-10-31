b7160b9bcb8fb34891bf1321ac736d252aa7d1e3
Sakshi Bisen <sakshi.bisen45@gmail.com>
2022-11-01 00:51:02 +0530

    ui for listing

--- rentals/src/components/Header.jsx
+++ rentals/src/components/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useState,useEffect } from 'react';
 import { useLocation ,useNavigate} from 'react-router'
+import {getAuth,onAuthStateChanged} from "firebase/auth"
 export default function Header() {
+  const [pageState,setpageState]=useState("Sign-in")
   const location=useLocation();
   const navigate=useNavigate();
+  const auth=getAuth();
+  useEffect(()=>{
+    onAuthStateChanged(auth,(user)=>{
+      if(user){
+        setpageState("Profile")
+      }
+      else{
+        setpageState("Sign-in")
+      }
+    })
+  },[auth])
   function pathMathRoute(route){
     if(route===location.pathname){
       return true
@@ -10,7 +24,7 @@ export default function Header() {
   }
   return (
   
-    <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
+    <div className='bg-white border-b shadow-sm sticky top-0 z-40'>
     <div className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
       <div>
            <img src="https://d3ds86a50t7cic.cloudfront.net/assets/6.4/public/default/frontend/mathrubhumi/images/popup-logo.webp" alt="logo" 
@@ -27,9 +41,10 @@ export default function Header() {
            className={` cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500"}`}
            onClick={()=>navigate("/offers")}>Offers</li>
           <li
-           className={` cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`}
-           onClick={()=>navigate("/sign-in")}
-           >SignIn</li>
+           className={` cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
+           ${(pathMathRoute("/sign-in") || pathMathRoute("/profile")) && "text-black border-b-red-500"}`}
+           onClick={()=>navigate("/profile")}
+           >{pageState}</li>
         </ul>
       </div>
       
--- rentals/src/pages/Profile.jsx
+++ rentals/src/pages/Profile.jsx
@@ -1,10 +1,15 @@
-import { getAuth } from 'firebase/auth'
+import { getAuth, updateProfile } from 'firebase/auth'
+import { updateDoc,doc } from 'firebase/firestore'
 import React from 'react'
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate ,Link} from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { db } from '../firebase'
+import {FcHome} from 'react-icons/fc'
 export default function Profile() {
   const auth=getAuth()
   const navigate=useNavigate()
+  const [changeDetail,setchangeDetail]=useState(false); //edit
   const [formData,setformData]=useState({
     name:auth.currentUser.displayName,
     email:auth.currentUser.email
@@ -14,23 +19,58 @@ export default function Profile() {
     auth.signOut()
     navigate("/")
   }
+  function onChange(e){
+    setformData((prevState)=>({
+      ...prevState,
+      [e.target.id]:e.target.value
+
+    }))
+  }
+  async function onsubmit(){
+      try {
+        if(auth.currentUser.displayName!==name){
+          //update display name in firebase auth
+          await updateProfile(auth.currentUser,{
+            displayName:name,
+          });
+          //update name in firestore
+          const docRef=doc(db,"users",auth.currentUser.uid)
+          await updateDoc(docRef,{name:name})
+        }
+        toast.success("Profile details updated")
+      } catch (error) {
+        toast.error("Could not update profile detail")
+      }
+  }
   return (
     <>
     <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
       <h1 className='text-3xl text-center mt-6 font-bold'>My Profile</h1>
       <div className='w-full md:w-[50%] mt-6 px-3'>
         <form>
-          <input type="text" id="name" value={name} disabled className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out'/>
+          <input type="text" id="name" value={name} disabled={!changeDetail} onChange={onChange} className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${changeDetail &&" bg-red-200 focus:bg-red-200"}`}/>
           <input type="email" id="email" value={email} disabled className=' mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out'/>
           <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
             <p className='flex items-center mb-6 '> Do you want to change your name?
-              <span className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>
-                Edit
+              <span 
+              className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'
+              onClick={()=>
+             { changeDetail && onsubmit();  //edit==true and submit==true
+              setchangeDetail((prevState)=>!prevState)}
+             } >
+               {changeDetail?"Apply changes":"Edit"}
               </span>
             </p>
             <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer'> Sign Out</p>
           </div>
         </form>
+        <button type="submit" className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 '>
+          <Link to="/create-listing" className="flex justify-center items-center">
+          <FcHome className='mr-2 text-3xl bg-red-200 rounded-full p-1 border-2'/>
+          Sell or rent your home
+          </Link>
+          
+        </button>
       </div>
     </section>
     </>
