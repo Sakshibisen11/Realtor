7c34f717c04a0cbcecfadf2faf17b92df29c21aa
Sakshi Bisen <sakshi.bisen45@gmail.com>
2022-11-07 21:14:44 +0530

    Contact Landlord

--- rentals/src/App.js
+++ rentals/src/App.js
@@ -11,6 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import EditListing from "./pages/EditListing";
+import Listing from "./pages/Listing";
 function App() {
   return (
     <>
@@ -24,6 +25,8 @@ function App() {
         <Route path="/sign-in" element={<Signin/>}/>
         <Route path="/sign-up" element={<Signup/>}/>
         <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/category/:categoryName/:listingId" element={<Listing/>}/>
+        
         <Route path="/offers" element={<Offers/>}/> 
         <Route path="create-listing" element={<PrivateRoute/>}>
         <Route path="/create-listing" element={<CreateListing/>}/>
