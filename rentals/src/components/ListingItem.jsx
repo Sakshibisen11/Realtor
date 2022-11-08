52636f3888fe99036eea5f170de622ca36c3ce8d
Sakshi Bisen <sakshi.bisen45@gmail.com>
2022-11-08 12:06:11 +0530

    completed offers page

--- rentals/src/components/ListingItem.jsx
+++ rentals/src/components/ListingItem.jsx
@@ -6,7 +6,7 @@ import {AiFillEdit} from 'react-icons/ai'
 import {FaTrashAlt} from 'react-icons/fa'
 export default function ListingItem({listing,id,onEdit,onDelete}) {
   return (
-    <li className=' relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px'>
+    <li className=' relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]'>
         <Link className='contents' to={`/category/${listing.type}/${id}`}>
         <img className='h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in' loading="lazy" src={listing.imgUrls[0]}/>
         <Moment fromNow className=" absolute top-2 left-2 bg-[#3377cc] text-white uppercase text-xs font-semibold roundd-md px-2 py-1 shadow-lg ">
--- rentals/src/pages/Profile.jsx
+++ rentals/src/pages/Profile.jsx
@@ -116,7 +116,7 @@ export default function Profile() {
       {!loading && listings.length>0 &&(
         <>
         <h2 className='text-2xl text-center font-semibold mb-6 mt-6'> My Listings</h2>
-        <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 space-x-6 items-center justify-center'>
+        <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl-grid-cols-5 mx-auto space-x-6 items-center justify-center'>
           {listings.map((listing)=>(
             <ListingItem key={listing.id} id={listing.id} listing={listing.data} onDelete={()=>onDelete(listing.id)}  onEdit={()=>onEdit(listing.id)} />
           ))}
