982dea194092f8215cefcf43760bcfffcfc56ffe
Sakshi Bisen <sakshi.bisen45@gmail.com>
2022-11-08 12:18:12 +0530

    category page

--- rentals/src/pages/Home.jsx
+++ rentals/src/pages/Home.jsx
@@ -110,7 +110,7 @@ export default function Home() {
 {saleListings && saleListings.length>0 && (
         <div className='m-2 mb-6 ' >
           <h2 className='px-3 text-2xl mt-6 font-semibold'>Places for Sell</h2>
-          <Link to="/category/sell"><p className='px-3 text-sm text-blue-600 hover:text-blue-800 transition  duration-150 ease-in-out'>Show more places for sell</p></Link>
+          <Link to="/category/sale"><p className='px-3 text-sm text-blue-600 hover:text-blue-800 transition  duration-150 ease-in-out'>Show more places for sell</p></Link>
           <ul className='sm:grid sm:grid-cols lg:grid-cols-3 xl:grid-cols-4 space-x-4'>
             {saleListings.map((listing)=>(
               <ListingItem key={listing.id} listing={listing.data} id={listing.id}/>
